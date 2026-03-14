26dd91230158ef106eca471b897f35948042d0df
Kretostan <kubakret82@icloud.com>
2026-03-14 11:32:58 +0100

    fix: set position static at medium screen width

--- frontend/src/components/layout/navigation/Navigation.tsx
+++ frontend/src/components/layout/navigation/Navigation.tsx
@@ -14,6 +14,8 @@ const Navigation = () => {
     setNavHeight(`[${height}px]`);
   }, []);
 
+  /* TODO: Zrobić smooth scroll do danej sekcji */
+
   return (
     <nav
       className="fixed top-0 flex justify-center items-center w-screen bg-surface-section border-b border-border-default shadow-[0_0_10px_2px_black] z-1000"
@@ -21,9 +23,8 @@ const Navigation = () => {
     >
       <div className="relative flex justify-between items-center px-6 max-w-300 w-full">
         <Title />
-        {/* TODO: Zrobić smooth scroll do danej sekcji */}
         <MobileList height={navHeight} />
-        <div className="sm:static lg:absolute lg:left-1/2 lg:-translate-x-1/2 hidden md:block">
+        <div className="md:static lg:absolute lg:left-1/2 lg:-translate-x-1/2 hidden md:block">
           <List />
         </div>
         <div className="flex gap-2 md:gap-4 py-4">
