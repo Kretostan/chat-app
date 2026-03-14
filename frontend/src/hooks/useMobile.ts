f6b639c12ba410e336e51845a8abb4421a63a7d0
Kretostan <kubakret82@icloud.com>
2026-03-14 14:02:29 +0100

    feat: add mobile screen detecion hook

---
+++ frontend/src/hooks/useMobile.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export const useMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth >= 768);
+  useEffect(() => {
+    window.addEventListener("resize", () =>
+      setIsMobile(window.innerWidth <= 768),
+    );
+  }, []);
+
+  return isMobile;
+};
