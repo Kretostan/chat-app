d06e809973bffb4ea454ad4c050ff8eab48310ca
Kretostan <kubakret82@icloud.com>
2026-03-26 13:53:26 +0100

    fix: correct isMobile logic for mobile screen width

--- frontend/src/hooks/useMobile.ts
+++ frontend/src/hooks/useMobile.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useMobile = (): boolean => {
-  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth >= 768);
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 768);
   useEffect(() => {
     window.addEventListener("resize", () =>
       setIsMobile(window.innerWidth <= 768),
