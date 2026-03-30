75028686ebbb13dcad7b9aace513a42d6a1c21f4
Kretostan <kubakret82@icloud.com>
2026-03-30 07:34:04 +0200

    fix: add SVG heart icon

--- frontend/src/components/home/footer/Copyright.tsx
+++ frontend/src/components/home/footer/Copyright.tsx
@@ -1,22 +1,16 @@
-const Copyright = () => {
-  const year = new Date().getFullYear();
+import HeartIcon from "@/assets/heart.svg?react";
 
-  return (
-    <div className="flex flex-col items-center gap-3 py-6 max-w-300 w-full border-t border-white/25">
-      <p className="text-sm">&copy; {year} ChatApp. All rights reserved.</p>
-      <p className="text-xs">
-        Build with 💖 by{" "}
-        <a
-          href="https://github.com/Kretostan"
-          target="_blank"
-          rel="noopener noreferer"
-          className="text-primary"
-        >
-          Jakub Kret
-        </a>
-      </p>
-    </div>
-  );
+const Copyright = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		<div className="flex flex-col items-center gap-3 py-6 max-w-300 w-full border-t border-white/25">
+			<p className="text-sm">&copy; {year} ChatApp. All rights reserved.</p>
+			<p className="flex items-center text-xs">
+				Build with <HeartIcon height="18px" width="24px" /> by Jakub Kret
+			</p>
+		</div>
+	);
 };
 
 export default Copyright;
