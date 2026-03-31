45af23d77133926ddcadaa310f65c355fd83e6d3
Kretostan <kubakret82@icloud.com>
2026-03-31 12:54:56 +0200

    refactor: simplify Menu rendering by removing wrapper component
    - migrate Menu logic to parent component
    - use useMobile and useElementHeight hooks to handle navigation display

--- frontend/src/components/layout/navigation/Navigation.tsx
+++ frontend/src/components/layout/navigation/Navigation.tsx
@@ -1,35 +1,32 @@
-import { useEffect, useRef, useState } from "react";
-import Menu from "./Menu";
+import { useRef } from "react";
+import { useElementHeight, useMobile } from "@/hooks";
+import List from "./List";
+import MobileList from "./MobileList";
 import OutlineButton from "./OutlineButton";
 import PrimaryButton from "./PrimaryButton";
 import Title from "./Title";
 
 const Navigation = () => {
-	const [navHeight, setNavHeight] = useState<number | undefined>(undefined);
 	const navRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		const height = navRef?.current?.getBoundingClientRect().height;
-		setNavHeight(height);
-	}, []);
+	const isMobile = useMobile();
+	const navHeight = useElementHeight(navRef);
 
 	/* TODO: Zrobić smooth scroll do danej sekcji */
-
-  return (
-    <nav
-      className="fixed top-0 flex justify-center items-center w-screen bg-surface-section border-b border-border-default shadow-[0_0_10px_2px_black] z-1000"
-      ref={navRef}
-    >
-      <div className="relative flex justify-between items-center px-6 max-w-300 w-full">
-        <Title />
-        <Menu height={navHeight} />
-        <div className="flex gap-2 md:gap-4 py-4">
-          <OutlineButton>Login</OutlineButton>
-          <PrimaryButton>Sign Up</PrimaryButton>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav
+			className="fixed top-0 flex justify-center items-center w-full bg-surface-section border-b border-border-default shadow-[0_0_10px_2px_black] z-1000"
+			ref={navRef}
+		>
+			<div className="relative flex justify-between items-center px-6 max-w-300 w-full">
+				<Title />
+				{isMobile ? <MobileList height={navHeight} /> : <List />}
+				<div className="flex gap-2 md:gap-4 py-4">
+					<OutlineButton>Login</OutlineButton>
+					<PrimaryButton>Sign Up</PrimaryButton>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navigation;
