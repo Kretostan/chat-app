fa3f3342794b3c78cdad5bdd2673e4ee4f1f96ee
Kretostan <kubakret82@icloud.com>
2026-04-02 23:42:58 +0200

    refactor: add Link from TanStack Router

--- frontend/src/components/layout/navigation/List.tsx
+++ frontend/src/components/layout/navigation/List.tsx
@@ -1,77 +1,80 @@
 import { motion } from "framer-motion";
+import { Link } from "@tanstack/react-router";
 import { useMobile } from "@/hooks";
 
 const sections = [
-	{ label: "Features", href: "#features" },
-	{ label: "Why Us", href: "#stats" },
-	{ label: "Get Started", href: "#cta" },
-	{ label: "Contact", href: "#contact" },
+  { label: "Features", href: "features" },
+  { label: "Why Us", href: "stats" },
+  { label: "Get Started", href: "cta" },
+  { label: "Contact", href: "contact" },
 ];
 
 const menuVariants = {
-	initial: (isMobile: boolean) => ({
-		opacity: isMobile ? 0 : 1,
-		height: isMobile ? 0 : "auto",
-	}),
-	animate: {
-		opacity: 1,
-		height: "auto",
-		transition: {
-			staggerChildren: 0.1,
-		},
-	},
-	exit: (isMobile: boolean) => ({
-		opacity: isMobile ? 0 : 1,
-		height: isMobile ? 0 : "auto",
-	}),
+  initial: (isMobile: boolean) => ({
+    opacity: isMobile ? 0 : 1,
+    height: isMobile ? 0 : "auto",
+  }),
+  animate: {
+    opacity: 1,
+    height: "auto",
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  exit: (isMobile: boolean) => ({
+    opacity: isMobile ? 0 : 1,
+    height: isMobile ? 0 : "auto",
+  }),
 };
 
 const itemVariants = {
-	initial: (isMobile: boolean) => ({
-		opacity: isMobile ? 0 : 1,
-	}),
-	animate: {
-		opacity: 1,
-	},
-	exit: (isMobile: boolean) => ({
-		opacity: isMobile ? 0 : 1,
-	}),
+  initial: (isMobile: boolean) => ({
+    opacity: isMobile ? 0 : 1,
+  }),
+  animate: {
+    opacity: 1,
+  },
+  exit: (isMobile: boolean) => ({
+    opacity: isMobile ? 0 : 1,
+  }),
 };
 
 const hoverEffect = { color: "var(--primary)" };
 
 const List = ({ height }: { height?: number | undefined }) => {
-	const isMobile = useMobile();
+  const isMobile = useMobile();
 
-	const styles = isMobile
-		? `gap-8 flex-col absolute left-0 w-full bg-surface-section`
-		: "gap-6 lg:gap-12 flex-row justify-center static lg:absolute lg:left-1/2 lg:-translate-x-1/2";
-	const inlineStyles = isMobile ? { top: `${height}px` } : {};
+  const styles = isMobile
+    ? `gap-8 flex-col absolute left-0 w-full bg-surface-section`
+    : "gap-6 lg:gap-12 flex-row justify-center static lg:absolute lg:left-1/2 lg:-translate-x-1/2";
+  const inlineStyles = isMobile ? { top: `${height}px` } : {};
 
-	return (
-		<motion.ul
-			layout
-			custom={isMobile}
-			variants={menuVariants}
-			initial="initial"
-			animate="animate"
-			exit="exit"
-			className={`flex p-6 overflow-hidden ${styles}`}
-			style={inlineStyles}
-		>
-			{sections.map((section) => (
-				<motion.li
-					custom={isMobile}
-					variants={itemVariants}
-					whileHover={hoverEffect}
-					className="cursor-pointer"
-					key={section.label}
-				>
-					<a href={section.href}>{section.label}</a>
-				</motion.li>
-			))}
-		</motion.ul>
-	);
+  return (
+    <motion.ul
+      layout
+      custom={isMobile}
+      variants={menuVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className={`flex p-6 overflow-hidden ${styles}`}
+      style={inlineStyles}
+    >
+      {sections.map((section) => (
+        <motion.li
+          custom={isMobile}
+          variants={itemVariants}
+          whileHover={hoverEffect}
+          className="cursor-pointer"
+          key={section.label}
+        >
+          <Link to="/" hash={section.href}>
+            {section.label}
+          </Link>
+        </motion.li>
+      ))}
+    </motion.ul>
+  );
 };
 
 export default List;
