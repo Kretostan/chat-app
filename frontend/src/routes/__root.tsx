c29344073d4ab5eec2e2b460806bd8c37a1e507e
Kretostan <kubakret82@icloud.com>
2026-04-02 23:04:37 +0200

    feat: add simple not found and error component

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,6 +3,8 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,
+  errorComponent: () => <div>Error</div>,
+  notFoundComponent: () => <div>Not Found 404</div>,
 });
 
 function RootComponent() {
