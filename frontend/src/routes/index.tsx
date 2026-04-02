47bfeb34ba8ec84e76693f4fdb5ca9416828f3df
Kretostan <kubakret82@icloud.com>
2026-04-02 23:39:57 +0200

    refactor: migrate App component to index route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
-import App from "../App.tsx";
+import Cta from "@/components/home/cta/Cta";
+import Features from "@/components/home/features/Features";
+import Footer from "@/components/home/footer/Footer";
+import Hero from "@/components/home/Hero";
+import Stats from "@/components/home/stats/Stats";
+import Navigation from "@/components/layout/navigation/Navigation";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <App />;
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <main className="flex flex-col justify-center items-center">
+        <Features />
+        <Stats />
+        <Cta />
+      </main>
+      <Footer />
+    </>
+  );
 }
