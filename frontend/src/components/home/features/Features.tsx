ea00497b2f330ce9bfca5d8f3528624bb8dc364c
Kretostan <kubakret82@icloud.com>
2026-04-02 23:42:24 +0200

    refactor: change div to section and add section id

--- frontend/src/components/home/features/Features.tsx
+++ frontend/src/components/home/features/Features.tsx
@@ -2,24 +2,27 @@ import { FEATURES } from "@/data/features";
 import FeatureField from "./FeatureField";
 
 const Features = () => {
-	return (
-		<div className="flex flex-col items-center bg-surface-section w-full px-6 text-center pt-20 pb-16">
-			<div>
-				<h4 className="text-3xl/12 md:text-5xl/18 font-bold">
-					Everything you need to stay connected
-				</h4>
-				<p className="mt-8 mb-20 text-foreground-secondary">
-					Powerful features designed to enhance your team&apos;s productivity
-					and collaboration
-				</p>
-			</div>
-			<div className="flex flex-col md:flex-row justify-center gap-10 flex-wrap max-w-300 w-full">
-				{FEATURES.map(({ id, ...rest }) => (
-					<FeatureField key={id} {...rest} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <section
+      id="features"
+      className="flex flex-col items-center scroll-mt-20 bg-surface-section w-full px-6 text-center pt-20 pb-16"
+    >
+      <div>
+        <h4 className="text-3xl/12 md:text-5xl/18 font-bold">
+          Everything you need to stay connected
+        </h4>
+        <p className="mt-8 mb-20 text-foreground-secondary">
+          Powerful features designed to enhance your team&apos;s productivity
+          and collaboration
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center gap-10 flex-wrap max-w-300 w-full">
+        {FEATURES.map(({ id, ...rest }) => (
+          <FeatureField key={id} {...rest} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Features;
--- frontend/src/components/home/footer/Footer.tsx
+++ frontend/src/components/home/footer/Footer.tsx
@@ -4,7 +4,10 @@ import Socials from "./Socials";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center gap-8 pt-12 px-6 bg-[#0d0d0d] text-foreground-secondary">
+    <section
+      id="contact"
+      className="flex flex-col items-center gap-8 scroll-mt-20 pt-12 px-6 bg-[#0d0d0d] text-foreground-secondary"
+    >
       <h4 className="text-3xl text-primary font-semibold">ChatApp</h4>
       <p className="">Modern communication for modern teams</p>
       <ul className="flex flex-col sm:flex-row items-center gap-4 text-sm">
@@ -17,7 +20,7 @@ const Footer = () => {
       </ul>
       <Socials />
       <Copyright />
-    </div>
+    </section>
   );
 };
 
