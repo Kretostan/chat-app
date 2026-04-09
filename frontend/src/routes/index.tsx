311feef7562ab72b256a870c9cb8415a0805a482
Kretostan <kubakret82@icloud.com>
2026-04-09 15:02:10 +0200

    fix: add dynamic button

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,28 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import Cta from "@/components/home/cta/Cta";
 import Features from "@/components/home/features/Features";
-import Footer from "@/components/home/footer/Footer";
 import Hero from "@/components/home/Hero";
 import Stats from "@/components/home/stats/Stats";
 import Navigation from "@/components/layout/navigation/Navigation";
+import Footer from "@/components/layout/footer/Footer";
+import { useMobile } from "@/hooks";
+import List from "@/components/home/navigation/List";
+import MobileList from "@/components/home/navigation/MobileList";
+import PrimaryButton from "@/components/layout/navigation/PrimaryButton";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const isMobile = useMobile();
+  const navigate = useNavigate({ from: "/" });
   return (
     <>
-      <Navigation />
+      <Navigation>
+        {isMobile ? <MobileList /> : <List />}
+        <PrimaryButton onClick={() => navigate({ to: "/auth" })}>Go in</PrimaryButton>
+      </Navigation>
       <Hero />
       <main className="flex flex-col justify-center items-center">
         <Features />
