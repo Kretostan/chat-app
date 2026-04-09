3eb8473b34aa6576ce1f1500431cdaae10712f6f
Kretostan <kubakret82@icloud.com>
2026-04-09 15:00:31 +0200

    feat: sample auth page layout

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -1,3 +1,6 @@
+import Socials from "@/components/home/footer/Socials";
+import Footer from "@/components/layout/footer/Footer";
+import Navigation from "@/components/layout/navigation/Navigation";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/auth")({
@@ -5,5 +8,13 @@ export const Route = createFileRoute("/auth")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/auth"!</div>;
+  return <>
+    <Navigation />
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h2>Auth Page</h2>
+    </div>
+    <Footer>
+      <Socials />
+    </Footer>
+  </>;
 }
