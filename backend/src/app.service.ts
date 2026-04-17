26170946b55f806eec9785d2b7abba6258227204
Kretostan <kubakret82@icloud.com>
2026-04-17 11:11:18 +0200

    feat: return object with name and surname field

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
+  getHello(): { name: string, surname: string } {
+    return { name: "John", surname: "Wick" };
   }
 
   healthCheck(): { status: string } {
