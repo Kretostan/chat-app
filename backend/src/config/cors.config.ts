9ea4cc1bed237df178510471fd92cad37972a35a
Kretostan <kubakret82@icloud.com>
2026-05-12 17:55:33 +0200

    refactor: remove CORS config

--- backend/src/config/cors.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from "@nestjs/config";
-
-export default registerAs("cors", () => ({
-  whitelist: [
-    process.env.APP_URL,
-    "http://localhost:5173",
-    "http://localhost:8080"
-  ].filter(Boolean)
-}));
