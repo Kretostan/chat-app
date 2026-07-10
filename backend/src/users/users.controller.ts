2eb0292b04c98f4b317e0191fad332480924bfdb
Jakub Kret <kubakret82@icloud.com>
2026-07-11 00:17:43 +0200

    feat(users): add search endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { UsersService } from "./users.service";
 
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get("/search")
+  @UseGuards(JwtAuthGuard)
+  async findUsers(@Query("q") query: string) {
+    return this.usersService.findUsers(query);
+  }
 }
