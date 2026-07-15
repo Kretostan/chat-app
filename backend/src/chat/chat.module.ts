cd2c0e55a2185e6bb8022b3feb9d7713ace2ca44
Jakub Kret <kubakret82@icloud.com>
2026-07-15 22:48:04 +0200

    feat(auth): add wsJwt guard, wiring and deps

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "src/auth/auth.module";
 import { ChatController } from "./chat.controller";
+import { ChatGateway } from "./chat.gateway";
 import { ChatService } from "./chat.service";
+import { WsService } from "./ws.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway, WsService],
 })
 export class ChatModule {}
