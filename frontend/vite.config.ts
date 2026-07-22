29f49e96a0868cb3d244e04259d4726935619ab3
Jakub Kret <kubakret82@icloud.com>
2026-07-22 16:29:34 +0200

    test(frontend): add Vitest setup and tests for Button component & useAuthForm hook

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,14 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   if (!Object.values(env).length) {
     throw new Error(
-      "File with environment variables in specific mode is probably missing!",
+      "File with environment variables in specific mode is probably not loaded!",
     );
   }
 
@@ -46,6 +47,13 @@ export default defineConfig(({ mode }) => {
           ws: true,
         },
       },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./src/__tests__/setup.ts", "./src/vitest-setup.d.ts"],
+      include: ["**/*.test.{ts,tsx}"],
+      exclude: [...configDefaults.exclude, "**node_modules/**"],
     },
   };
 });
