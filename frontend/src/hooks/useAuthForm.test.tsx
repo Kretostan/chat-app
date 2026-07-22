af444bb81637e912fcea6854125d8941e7fa8e76
Jakub Kret <kubakret82@icloud.com>
2026-07-22 19:18:09 +0200

    chore(test): remove unnecessary import

--- frontend/src/hooks/useAuthForm.test.tsx
+++ frontend/src/hooks/useAuthForm.test.tsx
@@ -1,5 +1,4 @@
 import { act, renderHook } from "@testing-library/react";
-import * as React from "react";
 import { describe, expect, it } from "vitest";
 import { useAuthForm } from "./useAuthForm";
 
